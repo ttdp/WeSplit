60712a5ff923c1b9186f9ca025ca81178058c1c4
ttdp <tiantong.dev@gmail.com>
2019-10-13 09:17:32 +0800

    Initial commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Tian Tong on 2019/10/13.
+//  Copyright © 2019 TTDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
